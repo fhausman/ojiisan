2976ef08cdf6008929afd1cec683f0845a04cc04
Filip Hausman <filip.hausman@gmail.com>
2021-01-29 01:37:54 +0100

    scripts added

---
+++ Assets/Scripts/Furnace.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Furnace : MonoBehaviour
+{
+    [SerializeField]
+    private float _initialHeat = 100.0f;
+
+    [SerializeField]
+    private float _coolingDiff = 5.0f;
+
+    [SerializeField]
+    private float _heatingDiff = 10.0f;
+
+    [SerializeField]
+    private float _coolingInterval = 10.0f;
+
+    [SerializeField]
+    private UnityEvent _onHeatZeroed;
+
+    private float _currentHeat = 0.0f;
+    private float _timer = 0.0f;
+
+    void Start()
+    {
+        _currentHeat = _initialHeat;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_timer >= _coolingInterval)
+        {
+            _currentHeat -= _coolingDiff;
+            _timer = 0.0f;
+        }
+        else
+        {
+            _timer += Time.deltaTime;
+        }
+
+        if(_currentHeat <= 0.0f)
+        {
+            _onHeatZeroed.Invoke();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Cane"))
+        {
+            if (_currentHeat < _initialHeat)
+            {
+                _timer = 0.0f;
+                _currentHeat += _heatingDiff;
+                if(_currentHeat > _initialHeat)
+                {
+                    _currentHeat = _initialHeat;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MainGameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameManager : MonoBehaviour
+{
+    [SerializeField]
+    private int _score = 0;
+
+    [SerializeField]
+    private Furnace[] furnaces = null;
+
+    public void OnGameLost()
+    {
+        //Show lose screen
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
