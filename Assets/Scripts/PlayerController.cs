e37202ef01633d19cb6f077d2877745dad08f48f
Filip Hausman <filip.hausman@gmail.com>
2021-01-29 03:55:30 +0100

    intro movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,247 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+    Idle,
+    Walk,
+    Jump,
+    Falling
+}
+
+public class PlayerIdle : BaseState
+{
+    public PlayerController pc;
+
+    public override void onUpdate(float deltaTime)
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            pc.StateMachine.ChangeState(PlayerState.Jump);
+        }
+
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f)
+        {
+            pc.StateMachine.ChangeState(PlayerState.Walk);
+        }
+    }
+
+    public override void onFixedUpdate(float deltaTime)
+    {
+        if (!pc.Grounded)
+        {
+            pc.StateMachine.ChangeState(PlayerState.Falling);
+        }
+        var velocity = pc.Rigidbody.velocity;
+        velocity -= -pc.Friction * pc.transform.localScale.x * Vector2.right;
+        velocity = new Vector2(
+            pc.transform.localScale.x < 0.0f ? Mathf.Clamp(velocity.x, 0.0f, pc.WalkSpeed) : Mathf.Clamp(velocity.x, -pc.WalkSpeed, 0.0f),
+            velocity.y
+            );
+        pc.Rigidbody.velocity = velocity;
+    }
+}
+
+public class PlayerWalk : BaseState
+{
+    public PlayerController pc;
+
+    public override void onUpdate(float deltaTime)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            pc.StateMachine.ChangeState(PlayerState.Jump);
+        }
+
+        var input = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(input) < 0.1f)
+        {
+            pc.StateMachine.ChangeState(PlayerState.Idle);
+        }
+    }
+
+    public override void onFixedUpdate(float deltaTime)
+    {
+        pc.UpdateHorizontal();
+        if (!pc.Grounded)
+        {
+            pc.StateMachine.ChangeState(PlayerState.Falling);
+        }
+    }
+}
+
+public class PlayerJump : BaseState
+{
+    public PlayerController pc;
+
+    private float timeElapsed = 0.0f;
+
+    public override void onInit(params object[] args)
+    {
+        timeElapsed = 0.0f;
+    }
+
+    public override void onUpdate(float deltaTime)
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            pc.StateMachine.ChangeState(PlayerState.Falling);
+            return;
+        }
+    }
+
+    public override void onFixedUpdate(float deltaTime)
+    {
+        if(Physics2D.Raycast(pc.transform.position, Vector2.up, 1.75f, LayerMask.GetMask("Ground"))
+            || timeElapsed >= pc.JumpTime)
+        {
+            pc.StateMachine.ChangeState(PlayerState.Falling);
+            return;
+        }
+
+        pc.UpdateHorizontal();
+        pc.Rigidbody.velocity = new Vector3(pc.Rigidbody.velocity.x, pc.JumpForce);
+
+        timeElapsed += deltaTime;
+    }
+}
+
+public class PlayerFall : BaseState
+{
+    public PlayerController pc;
+
+    public override void onInit(params object[] args)
+    {
+    }
+
+    public override void onFixedUpdate(float deltaTime)
+    {
+        if (pc.Grounded)
+        {
+            pc.StateMachine.ChangeState(PlayerState.Idle);
+            return;
+        }
+
+        pc.UpdateHorizontal();
+        if (pc.Rigidbody.velocity.y > 0.0f)
+        {
+            pc.Rigidbody.velocity -= new Vector2(0.0f, 5.0f);
+        }
+        else
+        {
+            pc.Rigidbody.velocity = new Vector2(pc.Rigidbody.velocity.x, -pc.FallForce);
+        }
+    }
+}
+
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float _jumpForce = 10.0f;
+    public float JumpForce { get => _jumpForce; }
+
+    [SerializeField]
+    private float _jumpTime = 1.0f;
+    public float JumpTime { get => _jumpTime; }
+
+    [SerializeField]
+    private float _fallForce = 30.0f;
+    public float FallForce { get => _fallForce; }
+
+    [SerializeField]
+    private float _walkSpeed = 10.0f;
+    public float WalkSpeed { get => _walkSpeed; }
+
+    [SerializeField]
+    private float _friction = 5.0f;
+    public float Friction { get => _friction; }
+
+    private Rigidbody2D _rb = null;
+    public Rigidbody2D Rigidbody { get => _rb; }
+
+    private SpriteRenderer _sr = null;
+    public SpriteRenderer SpriteRenderer { get => _sr; }
+
+    private StateMachine<PlayerState> _stateMachine = new StateMachine<PlayerState>();
+    public StateMachine<PlayerState> StateMachine { get => _stateMachine; }
+
+    #region Blur related properties
+    [SerializeField]
+    private GameObject _rendTex = null;
+
+    [SerializeField]
+    private float _blurSpeed = 4.0f;
+
+    private Material _blur = null;
+    private bool _shouldBlur = false;
+    private float _blurValue = 0.0f;
+    #endregion
+
+    public bool Grounded
     {
-        
+        get => Physics2D.Raycast(transform.position - Vector3.right * 0.75f, Vector2.down, 2.05f, LayerMask.GetMask("Ground")) ||
+            Physics2D.Raycast(transform.position - Vector3.left * 0.75f, Vector2.down, 2.05f, LayerMask.GetMask("Ground"));
+    }
+
+    public void UpdateHorizontal()
+    {
+        var input = Input.GetAxis("Horizontal");
+        _rb.velocity = new Vector3(input * WalkSpeed, _rb.velocity.y);
+    }
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _sr = GetComponent<SpriteRenderer>();
+        _blur = _rendTex.GetComponent<Renderer>().material;
+
+        _stateMachine.AddState(PlayerState.Idle, new PlayerIdle() { pc = this });
+        _stateMachine.AddState(PlayerState.Walk, new PlayerWalk() { pc = this });
+        _stateMachine.AddState(PlayerState.Jump, new PlayerJump() { pc = this });
+        _stateMachine.AddState(PlayerState.Falling, new PlayerFall() { pc = this });
+        _stateMachine.ChangeState(PlayerState.Idle);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _stateMachine.OnUpdate(Time.deltaTime);
+        UpdateBlur();
+        UpdateDirection();
+    }
+
+    private void FixedUpdate()
+    {
+        _stateMachine.OnFixedUpdate(Time.fixedDeltaTime);
+    }
+
+    private void UpdateBlur()
+    {
+        if(_shouldBlur)
+        {
+            _blurValue += _blurSpeed * Time.deltaTime; 
+        }
+        else
+        {
+            _blurValue -= _blurSpeed * Time.deltaTime;
+        }
+        _blurValue = Mathf.Clamp(_blurValue, 0.0f, 4.0f);
+
+        _blur.SetFloat("_Size", _blurValue);
+    }
+
+    private void UpdateDirection()
+    {
+        var input = Input.GetAxis("Horizontal");
+        if (input > 0.0f + Mathf.Epsilon)
+        {
+            _rb.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
+        else if (input < 0.0f - Mathf.Epsilon)
+        {
+            _rb.transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        }
     }
 }
---
+++ Assets/Scripts/Utilities/StateMachine.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+public abstract class BaseState
+{
+    public virtual void onInit(params object[] args) {}
+    public virtual void onUpdate(float deltaTime) {}
+    public virtual void onFixedUpdate(float deltaTime) {}
+    public virtual void onExit() {}
+}
+
+public class EmptyState : BaseState
+{
+}
+
+public class StateMachine<T>
+{
+    public T CurrentState { get; private set; }
+    public bool AlreadyAdded(T s) { return _states.ContainsKey(s); }
+
+    private Dictionary<T, BaseState> _states = new Dictionary<T, BaseState>();
+    private BaseState _currentState = new EmptyState();
+
+    public void AddState(T id, BaseState state)
+    {
+        _states.Add(id, state);
+    }
+
+    public void ChangeState(T id, params object[] args)
+    {
+        CurrentState = id;
+        _currentState.onExit();
+        _currentState = _states[id];
+        _currentState.onInit(args);
+    }
+
+    public void OnUpdate(float deltaTime)
+    {
+        _currentState.onUpdate(deltaTime);
+    }
+
+    public void OnFixedUpdate(float deltaTime)
+    {
+        _currentState.onFixedUpdate(deltaTime);
+    }
+}
