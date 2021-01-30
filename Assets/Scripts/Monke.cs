ee38727595aa69908043bd76f6ea433ee5cd993b
Filip Hausman <filip.hausman@gmail.com>
2021-01-30 13:40:55 +0100

    monke wip

--- Assets/Scripts/Monke.cs
+++ Assets/Scripts/Monke.cs
@@ -2,6 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum MonkeStates
+{
+    Idle,
+    GoingForItem,
+    WalkingWithItem,
+    Attack,
+    Hit,
+}
+
 public class Monke : MonoBehaviour
 {
     // Start is called before the first frame update
