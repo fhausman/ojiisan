0a88564e633b02f73417bc7f15a78ad5c259a2ab
Filip Hausman <filip.hausman@gmail.com>
2021-01-31 05:00:00 +0100

    level

---
+++ Assets/Water.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Water : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Pickable"))
+        {
+
+        }
+        else if(collision.CompareTag("Player"))
+        {
+        
+        }
+    }
+}
