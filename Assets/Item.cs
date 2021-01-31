4d3725546643b0c0a6b350d6b011cd7ed6a24107
Filip Hausman <filip.hausman@gmail.com>
2021-01-31 02:46:11 +0100

    monke drop

--- Assets/Item.cs
+++ Assets/Item.cs
@@ -2,17 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+public class Item : MonoBehaviour, IPickable
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float _score = 100.0f;
+
+    [SerializeField]
+    private float _pickCooldown = 1.0f;
+    private float _cooldownElapsed = 1.0f;
+
+    private BoxCollider2D _pickTrigger = null;
+    private Rigidbody2D _rigidbody = null;
+    private SpriteRenderer _spriteRenderer = null;
+
+    public GameObject GetObjRef()
     {
-        
+        return gameObject;
     }
 
-    // Update is called once per frame
+    public void OnObjectDrop(GameObject owner)
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void OnObjectPick(PlayerController pc)
+    {
+        gameObject.SetActive(false);
+        //return to the pool
+    }
+
+    public void OnObjectPick(Monke monke)
+    {
+        monke.ItemSlot.SetActive(true);
+        monke.ItemSprite.sprite = _spriteRenderer.sprite;
+        monke.HeldObject = this;
+
+        gameObject.SetActive(false);
+        _pickTrigger.enabled = false;
+    }
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _pickTrigger = GetComponentsInChildren<BoxCollider2D>()[1];
+    }
+
     void Update()
     {
-        
+        _cooldownElapsed += Time.deltaTime;
+        if (_cooldownElapsed >= _pickCooldown)
+        {
+            _pickTrigger.enabled = true;
+        }
     }
 }
